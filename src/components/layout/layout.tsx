294686358aa910b11de05c546fdd21e3cdf4c0e6
leoroullois <roullois.leo@protonmail.com>
2022-11-11 15:17:56 +0100

    feat : scroll bar, back to top button

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,20 @@
 import {FC, PropsWithChildren} from 'react';
 import Footer from '@layout/footer';
+import BackToTop from '@components/_common/back-to-top';
+import StatusBar from '@components/_common/scroll-bar';
+import {useRouter} from 'next/router';
 
-const Layout: FC<PropsWithChildren> = ({children}) => (
-  <div className='flex flex-col justify-between bg-zinc-900 text-gray-50 font-sans min-h-screen h-full'>
-    {children}
-    <Footer />
-  </div>
-);
+const Layout: FC<PropsWithChildren> = ({children}) => {
+  const router = useRouter();
+
+  return (
+    <div className='flex flex-col justify-between bg-zinc-900 text-gray-50 font-sans min-h-screen h-full'>
+      <BackToTop />
+      {!!router.query.slug && <StatusBar />}
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
